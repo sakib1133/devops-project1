578eb032f97cabd3124f9a55d7ddfffa21cfdd30
sakib <oreo6864@gmail.com>
2026-03-12 10:16:15 +0000

    Add nginx log monitoring script

---
+++ log-check/log-check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "============================"
+echo "Nginx Log Monitoring"
+echo "============================"
+echo ""
+echo "Last 20 lines of Nginx Access Log:"
+sudo tail -n 20 /var/log/nginx/access.log
+echo ""
+echo "Last 20 lines of Nginx Error Log:"
+sudo tail -n 20 /var/log/nginx/error.log#!/bin/bash
+echo "============================"
+echo "Nginx Log Monitoring"
+echo "============================"
+echo ""
+echo "Last 20 lines of Nginx Access Log:"
+sudo tail -n 20 /var/log/nginx/access.log
+echo ""
+echo "Last 20 lines of Nginx Error Log:"
+sudo tail -n 20 /var/log/nginx/error.log
